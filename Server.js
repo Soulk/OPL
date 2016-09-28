ecc0e021c4c39028604605f93cd58e7b7d643678
pdf <fitoussi.david60@gmail.com>
2016-09-28 18:00:51 +0200

    Ajout de nodeJS (Express) + Template bootstrap Initialisation du server

---
+++ Server.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
+
+app.use(express.static('css'));
+app.use(express.static('fonts'));
+app.use(express.static('assets'));
+app.use(express.static('img'));
+
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/",function(req,res){
+  //res.setHeader("Content-Type", 'text/css');
+  res.sendFile(path + "index.html");
+});
+
+router.get("/about",function(req,res){
+  res.sendFile(path + "about.html");
+});
+
+router.get("/contact",function(req,res){
+  res.sendFile(path + "contact.html");
+});
+
+app.use("/",router);
+
+app.use("*",function(req,res){
+  res.sendFile(path + "404.html");
+});
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
