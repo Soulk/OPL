1305027ff69aecc4af5947899b9e207f010f2d81
soulk <fitoussi.david60@gmail.com>
2016-10-02 18:27:07 +0200

    -Changement du dashboard(affichage uniquement des repos avec au moins une pull request ouvertes) -Chargment des pulls requests au clique sur le nom du repo -Essaie sur l'écriture du ratio, formule à revoir (actuellement good pull request/(good pull request + bad pull request)) -Tous les liens sont à priori opérationnels et envoie sur la bonne zone de github

--- routes/index.js
+++ routes/index.js
@@ -9,19 +9,55 @@ var path = pathModule.resolve(__dirname, '../views');
 router.get('/',function(req, ser) {
         req.app.get('github').repos.getAll({
             sort : "updated"
-        }, function(err, res) {
+        }, function(err, response) {
             console.log("\n\nazeakejkaze kAJHJAZHJHJHJZRH JZRJKHZJR HHRJHZ JKZHRJKHZRJHZKRHZJRKZRHJHZR JZRKHZRJR");
-            console.log(res[1]);
-            ser.render("index.ejs",{
-                res : res
-            });
+            //console.log(response);
+           //console.log(response.length);
+           var lock = 0;
+           var arrayPull = [];
+            for(var i = 0 ;i< response.length; i++) {
+                var tmp = lock;
+                ser.app.get('github').pullRequests.getAll({
+                    user: response[i].owner.login,
+                    repo: response[i].name,
+                    state: "open",
+                    per_page: 100
+                 }, function(err, res) {
+                    lock++;
+                    console.log(res+" " + i);
+                    //c onsole.log(response[tmp]);
+                    arrayPull.push(res);
+                    if(lock == response.length) {
+                    //console.log(arrayPull);
+                    for(var j=0 ; j<response.length;j++) {
+                        for(var k =0;k<arrayPull.length;k++) {
+                            //console.log(arrayPull[k][0]);
+                            if(arrayPull[k][0] != undefined) {
+                                 console.log(arrayPull[k][0].html_url +" " + response[j].html_url );
+                                if( arrayPull[k][0].html_url.indexOf(response[j].html_url) != -1) {
+                                    console.log(arrayPull[k].length);
+                                    response[j].id = {id: response[j].id , nbpulls: arrayPull[k].length};
+                                    k = arrayPull.length+1;
+                                    break;
+                                }
+                            } else {
+                                response[j].id = {id: response[j].id , nbpulls: 0};
+                            }
+                        }
+                    }
+                        //console.log(response);
+                        ser.render("index.ejs",{
+                            res : response
+                        });
+                    }
+                 });
 
+            }
         });
 
             //res.sendFile(path + "/index.html");
 
 });
-
 
 router.get('/user.html',function(req, res) {
     res.sendFile(path + "/user.html");
--- routes/pullrequest.js
+++ routes/pullrequest.js
@@ -5,19 +5,76 @@ var router = express.Router();
 var pathModule = require("path");
 var path = pathModule.resolve(__dirname, '../views');
 
-router.get('/',function(req, res) {
-    res.sendFile(path + "/pullrequest.html");
+router.get('/',function(req, ser) {
      console.log("pullrequest");
      console.log(req.query.owner);
      console.log(req.query.repo);
-     req.app.get('github').pullRequests.getAll({
-        // user: req.query.owner,
-        // repo: req.query.repo
-        user: "foretagsplatsen",
-        repo: "numbro",
-        state: "closed"
+     ser.app.get('github').pullRequests.getAll({
+         user: req.query.owner,
+         repo: req.query.repo,
+        //user: "foretagsplatsen",
+        //repo: "numbro",
+        state: "all",
+        per_page: 100
      }, function(err, res) {
-         console.log(res);
+         //console.log(res);
+         var arrayUser=[];
+         //console.log(res.length);
+         for(var i=0; i<res.length; i++) {
+         //console.log(res[i].user.login);
+            var isAlready = false;
+            for(var j = 0 ; j <arrayUser.length; j++) {
+                if(res[i].user.login == arrayUser[j]) {
+                    isAlready = true;
+                }
+            }
+            if(!isAlready) {
+                arrayUser.push(res[i].user.login);
+            }
+         }
+         for(var i=0; i<arrayUser.length;i++) {
+            var good = 0;
+            var bad = 0;
+            for(var j=0; j<res.length; j++) {
+                if(res[j].user.login == arrayUser[i]) {
+                    if(res[j].merged_at == null && res[j].closed_at != null) {
+                        bad++;
+                        // console.log(res[j].user.login +" BAD");
+                    } else if(res[j].closed_at != null) {
+                        good++;
+                    }
+                }
+            }
+            //console.log("BAD = " + bad + " GOOD = " + good);
+            if(bad == 0 && good == 0) {
+                arrayUser[i] = { name: arrayUser[i], ratio:  "new", total: "new"};
+            } else {
+                arrayUser[i] = { name: arrayUser[i], ratio:  Math.floor((good/(good+bad))*100), total: good+bad};
+            }
+         }
+         var pullRequests = [];
+         for(var i=0; i<res.length; i++) {
+            //console.log(res[i].state);
+            if(res[i].state == 'open') {
+                for(var j = 0; j<arrayUser.length;j++) {
+                    //console.log(res[i].user.login+" " + arrayUser[j].name);
+                    if(res[i].user.login == arrayUser[j].name) {
+                        res[i].user.login = arrayUser[j];
+
+                        break;
+                    }
+                }
+                pullRequests.push(res[i]);
+            }
+         }
+         //console.log(arrayUser);
+         console.log(pullRequests);
+         console.log(req);
+         ser.render("pullrequest.ejs",{
+             res : pullRequests,
+             req : req
+         });
+
      });
 
 
